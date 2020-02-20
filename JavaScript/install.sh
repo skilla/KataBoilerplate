055ac5532dcd879c5240ac68c28a8fd977a1001c
Sergio Zambrano <sergio.zambrano@enalquiler.com>
2020-02-20 11:06:11 +0100

    improvements of JavaScript usage

---
+++ JavaScript/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker pull node:lts
+
+docker build -t nodeforkatas:latest .
+
+rm -rf node_modules
+rm -rf yarn.lock
+
+docker run -it --rm --name node -v "$PWD":/usr/src/app -w /usr/src/app nodeforkatas yarn add \
+    @babel/core \
+    @babel/preset-env \
+    babel-loader \
+    css-loader \
+    jest \
+    style-loader \
+    ts-loader \
+    typescript \
+    webpack \
+    webpack-cli

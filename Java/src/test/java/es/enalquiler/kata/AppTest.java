15fe726fe6b3cc126ac2c02b7b84ea703e482e1b
Sergio Zambrano <sergio.zambrano@enalquiler.com>
2020-03-03 15:48:11 +0100

    Java tests

--- Java/src/test/java/es/enalquiler/kata/AppTest.java
+++ Java/src/test/java/es/enalquiler/kata/AppTest.java
@@ -1,20 +1,44 @@
 package es.enalquiler.kata;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void checkOutputString()
     {
-        assertTrue( true );
+        final String expected =
+            "1 + 2 = 3\n" +
+            "3 - 1 = 2\n" +
+            "3 * 2 = 6\n" +
+            "3 / 2 = 1.5\n"
+        ;
+
+        App.main(new String[]{});
+        assertEquals(expected, outContent.toString());
     }
 }
